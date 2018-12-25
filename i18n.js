9f2414436dc330d1b3f7b2ed9fa670533c538e52
Diovane Gualberto da Silva <diovane@smbh-020389.local>
2018-12-25 01:17:48 -0200

    ios adjustments

--- i18n.js
+++ i18n.js
@@ -3,8 +3,9 @@ import i18n from 'i18n-js';
 
 import ptBR from './translations/pt-BR.json';
 
+i18n.defaultLocale = 'pt-BR';
 i18n.locale = RNLanguages.language;
-i18n.fallbacks = true;
+i18n.fallbacks = 'pt-BR';
 i18n.translations = { 'pt-BR': ptBR };
 
 export default i18n;
