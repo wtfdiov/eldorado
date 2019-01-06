7fbccf271cf71404acecb032c187d1be6e2529ac
Diovane Gualberto <diov@outlook.com>
2019-01-05 22:29:21 -0200

    English translation

--- i18n.js
+++ i18n.js
@@ -2,10 +2,14 @@ import RNLanguages from 'react-native-languages';
 import i18n from 'i18n-js';
 
 import ptBR from './translations/pt-BR.json';
+import enUS from './translations/en-US.json'
 
 i18n.defaultLocale = 'pt-BR';
 i18n.locale = RNLanguages.language;
 i18n.fallbacks = 'pt-BR';
-i18n.translations = { 'pt-BR': ptBR };
+i18n.translations = {
+  'pt-BR': ptBR,
+  'en-US': enUS
+};
 
 export default i18n;
